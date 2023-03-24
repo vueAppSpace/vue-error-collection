dc8cfa38304066d4307f14ce530c017ba6a31267
IFLS <c4816@foxmail.com>
2023-03-24 13:13:20 +0800

    feat: 修改d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -15,10 +15,11 @@ declare module "version";
 declare module "@vitejs/plugin-legacy";
 
 interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string;
+  // readonly VITE_APP_TITLE: string;
+  readonly VITE_BASE_URL: string;
+  readonly VITE_ENV: string;
   readonly VITE_APP_API_BASE_URL: string;
   readonly VITE_BUILD_SOURCEMAP: string;
-  readonly VITE_BUILD_DROP_CONSOLE: string;
 }
 
 interface ImportMeta {
