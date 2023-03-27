4414703751f20ad1ec2e046cc73871787404905f
YanivWang <YanivWang@outlook.com>
2023-03-27 10:22:38 +0800

    feat: plusTools bug修复

--- src/utils/native/plusTools.js
+++ src/utils/native/plusTools.js
@@ -2,7 +2,7 @@
  * @Author: YanivWang YanivWang@outlook.com
  * @Date: 2023-03-21 11:24:48
  * @LastEditors: YanivWang
- * @LastEditTime: 2023-03-24 18:03:25
+ * @LastEditTime: 2023-03-27 10:19:43
  * @FilePath: \lk-xinaohealth-h5\src\utils\native\plusTools.js
  * @Description: ****
  */
@@ -14,16 +14,16 @@ import { eServicePdfPreview } from "@/config/env";
 /**
  * 关闭当前 webview
  */
-export function closeWebView() {
+export const closeWebView = () => {
   console.log("关闭webview>>>>>>>>>>");
   var ws = plus.webview.currentWebview();
   plus.webview.close(ws);
-}
+};
 
 /**
  * 打开一个新的webview
  */
-export function openWebView({ params }) {
+export const openWebView = ({ params }) => {
   console.log("打开新webview>>>>>>>>>>");
 
   const { targetUrl, title = "", refreshTicket = false, extraParame = "" } = params;
@@ -46,43 +46,41 @@ export function openWebView({ params }) {
   });
   webview.loadURL(targetUrl);
 
-  plus.key.addEventListener(
-    "backbutton",
-    function () {
-      webview.canBack(function (e) {
-        if (e.canBack) {
-          webview.back();
-        } else {
-          webview.close();
-        }
-      });
-    },
-    false
-  );
+  const onBack = () => {
+    webview.canBack(e => {
+      plus.key.removeEventListener("backbutton", onBack);
+      if (e.canBack) {
+        webview.back();
+      } else {
+        webview.close();
+      }
+    });
+  };
 
-  //新打开得webview，添加到当前webview
+  plus.key.addEventListener("backbutton", onBack, false);
+
   var currentWebview = plus.webview.currentWebview();
   currentWebview.append(webview);
-}
+};
 
 /**
  * 打电话
  */
-export function callPhone({ params, successCB }) {
+export const callPhone = ({ params }) => {
   plus.device.dial(params.tel, true);
-}
+};
 
 /**
  * 扫描二维码
  */
-export function scanQRCode({ params, successCB, errorCB }) {
+export const scanQRCode = ({ params, successCB, errorCB }) => {
   //创建扫码dom节点
   const htmlStr = `<div id="__qrCodeUUID"></div>`;
-  function createNode(htmlStr) {
+  const createNode = htmlStr => {
     const dom = document.createElement("div");
     dom.innerHTML = htmlStr;
     return dom.firstElementChild;
-  }
+  };
   const scanNode = createNode(htmlStr);
   document.body.appendChild(scanNode);
 
@@ -97,11 +95,12 @@ export function scanQRCode({ params, successCB, errorCB }) {
     close();
     if (type === plus.barcode.QR) {
       console.log("识别二位码成功>>>");
-      successCB({
-        qrcode: reslut
-      });
+      successCB &&
+        successCB({
+          qrcode: reslut
+        });
     } else {
-      errorCB("不是合法二维码!");
+      errorCB && errorCB("不是合法二维码!");
     }
   };
   scanInstance.start();
@@ -111,7 +110,7 @@ export function scanQRCode({ params, successCB, errorCB }) {
   const bitmap1 = new plus.nativeObj.Bitmap("bitmap1");
   bitmap1.loadBase64Data(
     "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAASCAYAAABit09LAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAACqADAAQAAAABAAAAEgAAAABr6GS2AAAAoklEQVQoFYWSvQ6CQBAGL3aEh5COkhJLabEgFtfz7HY2WliYGOM5SyDZYpclmVyy33A/e5dS8JVS6kBJCWmGL0yuTJhX6c54NEWCET7whM6TzoRveMHJk/pVEHHwpI7wAbLkxZNaQtm0nDB7UkV4gx/MpkTx4AVmnZnipbc/kePDKDluj5IHZt9vuJLjK1SyfhTNVjdH6SvIJVxNQReRlof7B/t01YxY86eAAAAAAElFTkSuQmCC",
-    function () {
+    () => {
       console.log("bmp1.png load success!");
 
       const viewRealTop = statusbarHeight + 25;
@@ -145,23 +144,23 @@ export function scanQRCode({ params, successCB, errorCB }) {
           console.log("click return back btn!!!");
           close();
 
-          errorCB("用户取消扫码!");
+          errorCB && errorCB("用户取消扫码!");
         },
         false
       );
     },
-    function (e) {
-      console.log("bmp1.png load failed! ");
+    e => {
+      errorCB && errorCB("bmp1.png load failed! ");
     }
   );
 
-  function close() {
+  const close = () => {
     bitmap1.clear();
     view1.close();
     scanInstance.close();
     scanNode.remove();
-  }
-}
+  };
+};
 
 /**
  * 打开微信小程序
@@ -175,14 +174,14 @@ export const openWXMiniProgram = ({ params }) => {
 /**
  * 预览文件
  */
-export const previewFile = ({ params, successCB, errorCB }) => {
+export const previewFile = ({ params }) => {
   console.log("预览文件>>>>");
 
   const { serverUrl, fileName } = params || {};
-  params = {
-    targetUrl: `${eServicePdfPreview}?url=${encodeURIComponent(Base64.encode(serverUrl))}`
-  };
   loadBase64(scriptUrl.base64).then(() => {
+    params = {
+      targetUrl: `${eServicePdfPreview}?url=${encodeURIComponent(Base64.encode(serverUrl))}`
+    };
     openWebView({ params });
   });
 };
@@ -190,33 +189,33 @@ export const previewFile = ({ params, successCB, errorCB }) => {
 /**
  * 获取当前地理位置
  */
-export function queryLocation({ successCB, errorCB }) {
+export const queryLocation = ({ successCB, errorCB }) => {
   console.log("start获取地理位置>>>>>>");
   plus.geolocation.getCurrentPosition(
     p => {
       console.log("获取地理位置成功>>>", p);
       const { coords, address = {} } = p;
       const { country, province, city, district, street, postalCode, cityCode } = address;
-      successCB({ country, province, city, district, street, postalCode, cityCode });
+      successCB && successCB({ country, province, city, district, street, postalCode, cityCode });
     },
     err => {
       console.log("获取地理位置失败,", err);
-      errorCB("获取地理位置失败：", err);
+      errorCB && errorCB("获取地理位置失败：", err);
     },
     {
       enableHighAccuracy: true,
       timeout: 10000,
-      provider: "amap", //system
+      provider: "amap", //system amap
       geocode: true
     }
   );
-}
+};
 
 /**
  * 音频播放
  */
-export function audioPlay({ params, successCB, errorCB }) {
-  //path = "https://lk-webfont.oss-cn-beijing.aliyuncs.com/web/xinao-health/audio/correct_sound.mp3";
+export const audioPlay = ({ params, successCB, errorCB }) => {
+  //const localAudioId = "https://lk-webfont.oss-cn-beijing.aliyuncs.com/web/xinao-health/audio/correct_sound.mp3";
 
   const { localAudioId } = params;
   console.log("音频播放>>>", localAudioId);
@@ -227,14 +226,15 @@ export function audioPlay({ params, successCB, errorCB }) {
     },
     err => {
       console.log("play audio err", err);
+      errorCB && errorCB("play audio err" + err);
     }
   );
-}
+};
 
 /**
  *下载音频
  */
-export function audioDownload({ params, successCB, errorCB }) {
+export const audioDownload = ({ params, successCB, errorCB }) => {
   console.log("audioDownload>>>>", params);
 
   downLoadFile({
@@ -243,32 +243,36 @@ export function audioDownload({ params, successCB, errorCB }) {
     },
     successCB: localAudioId => {
       console.log("localAudioId>>>", localAudioId);
-      successCB({ localAudioId });
+      successCB && successCB({ localAudioId });
     },
-    errorCB: () => {}
+    errorCB: err => {
+      errorCB && errorCB("下载音频失败!" + err);
+    }
   });
-}
+};
 
 /**
  * 文件下载
  */
-export function downLoadFile({ params, successCB, errorCB }) {
+export const downLoadFile = ({ params, successCB, errorCB }) => {
   const { path } = params;
   console.log("下载文件网络地址>>>>", path);
   var dtask = plus.downloader.createDownload(
     path,
     {},
-    function (d, status) {
+    (d, status) => {
       if (status == 200) {
-        console.log("downLoadFile success file path >>>: " + d.filename);
-        successCB(d.filename);
+        console.log("downLoadFile success, file path >>>: ", d.filename);
+        successCB && successCB(d.filename);
       } else {
-        console.log("Download failed: " + status);
+        console.log("downLoadFile failed: ", status);
+        errorCB && errorCB("downLoadFile failed: " + status);
       }
     },
     err => {
-      console.log("Download failed: " + err);
+      console.log("downLoadFile failed: ", err);
+      errorCB && errorCB("downLoadFile failed: " + err);
     }
   );
   dtask.start();
-}
+};
