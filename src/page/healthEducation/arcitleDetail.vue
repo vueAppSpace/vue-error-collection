28ab27b4095d29c64e4dd0fd7a9dc587bb40ec24
yanghaifengb <yanghaifengb@enn.cn>
2023-03-31 14:36:23 +0800

    合并新开发的功能

--- src/page/healthEducation/arcitleDetail.vue
+++ src/page/healthEducation/arcitleDetail.vue
@@ -11,55 +11,55 @@
   </div>
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        educationAnswer: null
-      };
-    },
-    methods: {
-      getUserMissionFn() {
-        getUserMission()
-          .then(res => {
-            if (res.data) {
-              const regex = new RegExp("<img", "gi");
-              if (res.data.info) {
-                res.data.info = res.data.info.replace(regex, '<img style="max-width: 100%; height: auto"');
-              }
-              this.articleDetaile = res.data;
+export default {
+  data() {
+    return {
+      educationAnswer: null
+    };
+  },
+  methods: {
+    getUserMissionFn() {
+      getUserMission()
+        .then(res => {
+          if (res.data) {
+            const regex = new RegExp("<img", "gi");
+            if (res.data.info) {
+              res.data.info = res.data.info.replace(regex, '<img style="max-width: 100%; height: auto"');
             }
-          })
-          .catch(ex => {});
-      }
-    },
-    mounted() {
-      let educationAnswer = localStorage.getItem("educationAnswer");
-      const regex = new RegExp("<img", "gi");
-      if (educationAnswer) {
-        educationAnswer = educationAnswer.replace(regex, '<img style="max-width: 100%; height: auto"');
-        this.educationAnswer = educationAnswer;
-        this.zgStatistics("健康新奥-进入页面", {
+            this.articleDetaile = res.data;
+          }
+        })
+        .catch(ex => {});
+    }
+  },
+  mounted() {
+    let educationAnswer = localStorage.getItem("educationAnswer");
+    const regex = new RegExp("<img", "gi");
+    if (educationAnswer) {
+      educationAnswer = educationAnswer.replace(regex, '<img style="max-width: 100%; height: auto"');
+      this.educationAnswer = educationAnswer;
+      this.zgStatistics("健康新奥-进入页面", {
         所属功能模块: "服务",
         来源: "健康宣教",
-        页面名称: "文章详情",
+        页面名称: "文章详情"
       });
-      }
     }
-  };
+  }
+};
 </script>
 
 <style scoped lang="scss">
-  .container {
-    font-size: 30px;
-    font-weight: 400;
-    color: #333333;
-    background: #f7f9f9;
-    width: 750px;
-    margin: 0 auto;
-  }
-  .content {
-    background: #ffffff;
-    padding: 0 30px 0;
-    line-height: 1.6;
-  }
+.container {
+  font-size: 30px;
+  font-weight: 400;
+  color: #333333;
+  background: #f7f9f9;
+  width: 750px;
+  margin: 0 auto;
+}
+.content {
+  background: #ffffff;
+  padding: 0 30px 0;
+  line-height: 1.6;
+}
 </style>
