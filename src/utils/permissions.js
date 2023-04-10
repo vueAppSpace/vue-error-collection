a75060bbf30e5631a69d5b70a204f14987ff794d
IFLS <c4816@foxmail.com>
2023-04-10 15:56:36 +0800

    feat: 权限改造

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -2,7 +2,7 @@
  * @Description: 用户鉴权函数
  * @Author: IFLS
  * @Date: 2022-08-12 09:39:33
- * @LastEditTime: 2023-04-07 13:52:30
+ * @LastEditTime: 2023-04-10 15:56:00
  */
 import { Toast } from "vant";
 
@@ -22,21 +22,23 @@ function isEntropy() {
   return isTestUser === 2;
 }
 
+// TODO: 权限需要改造
 export default function () {
   const userStore = JSON.parse(sessionStorage.getItem("userStore"));
   if (!userStore) return throwError(userStore);
   const isTestUser = userStore.userInfo.isTestUser;
-  const isLangfang = userStore.userInfo.isLangfang;
+  // const isLangfang = userStore.userInfo.isLangfang;
 
   let user;
-  if (isTestUser === 0 && !isLangfang) {
-    user = 0; //  非廊坊普通用户
-  } else if (isTestUser === 0 && isLangfang) {
-    user = 1; // 廊坊普通用户
-  } else if (isTestUser > 0 && !isLangfang) {
-    user = 2; // 非廊坊测试用户
-  } else if (isTestUser > 0 && isLangfang) {
-    user = 3; // 廊坊测试用户
+  // 生命熵+健康计划用户
+  if (isTestUser === 2) {
+    user = 3;
+    // 健康计划用户
+  } else if (isTestUser === 1) {
+    user = 3;
+    // 工间操用户
+  } else if (isTestUser === 0) {
+    user = 0;
   } else {
     user = -1;
     throwError(userStore);
